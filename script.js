1c4847e67ef53ea07f86adb0f62cfeda0c976ade
connordennison <connor@dennison.email>
2020-08-21 18:46:06 +0100

    changed clock to act more like a real digital clock

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-vue = new Vue({
+/* vue = new Vue({
     el: "#root",
     data: {
     }
-})
+}) */
 
 setInterval(clockUpdate, 500);
 
 function clockUpdate(id) {
-    var dObj = new Date();
-    var h = dObj.getHours();
-    var m = dObj.getMinutes();
-    var s = dObj.getSeconds();
-
-    if (h > 12) {
-        h = h - 12;
-    }
-
-    document.getElementById("clock").innerHTML = `${h}:${m}:${s}`
+    const now = moment().format("HH:mm:ss")
+    document.getElementById("clock").innerHTML = `${now}`
 }
